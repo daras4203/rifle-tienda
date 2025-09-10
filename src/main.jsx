84c288f030be581eb83255a0ca7551733fdbb877
jose9709 <143565451+jose9709@users.noreply.github.com>
2025-09-10 11:35:26 -0500

    Creacion de las secciones

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
+    <Home />
+    <Footer />
   </StrictMode>,
 )
---
+++ src/routes/routerApp.jsx
@@ -0,0 +1,8 @@
+import Header from "../components/Header"
+import  Footer  from "../components/Footer"
+import Home from "../pages/Home"
+
+const routerApp = [
+  
+]
+   
