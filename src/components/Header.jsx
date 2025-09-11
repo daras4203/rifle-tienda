1495aba74771795d9edc32d0eb82aedba86f0198
Jhon ALexander David Hernandez <jdavidal@cesde.net>
2025-09-11 16:50:10 -0500

    Fix. Se reparan las vistas de  hombre y 'lo nuevo', además se agragan las rutas de navegacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,27 @@
+import { Link } from 'react-router-dom';
 import './Header.css'
 function Header() {
   return (
-    <div className='header'>
-      <a href="" className='logo-header'>RIFLE</a>
+    <div className="header">
+      <Link to="/" className="logo-header">
+        RIFLE
+      </Link>
       <nav>
-        <a href="" className="header-items">
+        <Link to="/loNuevo" className="header-items">
           LO NUEVO
-        </a>
-        <a href="" className="header-items">
+        </Link>
+        <Link to="/hombre" className="header-items">
           HOMBRE
-        </a>
-        <a href="" className="header-items">
+        </Link>
+        <Link to="/" className="header-items">
           MUJER
-        </a>
-        <a href="" className="header-items re">
+        </Link>
+        <Link to="/" className="header-items re">
           REBAJAS
-        </a>
+        </Link>
       </nav>
-      <div className='icons-nav'>
-        <a href="" className="icons">
+      <div className="icons-nav">
+        <Link to="/registro" className="icons">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -29,8 +32,8 @@ function Header() {
           >
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
           </svg>
-        </a>
-        <a href="" className="icons">
+        </Link>
+        <Link to="/" className="icons">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -41,7 +44,7 @@ function Header() {
           >
             <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z" />
           </svg>
-        </a>
+        </Link>
       </div>
     </div>
   );
